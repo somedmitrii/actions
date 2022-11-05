f866a0db613dc1c3a73e83ec6b1a42e99814a287
somedmitrii <somedmitrii@gmail.com>
2022-11-05 23:56:59 +0400

    added typescript, added linting in workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // var test = 123;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
